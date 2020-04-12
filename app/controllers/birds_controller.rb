ae215baa386c5c7277af26847059871e1346594e
kev-kev <kevin.clark93@gmail.com>
2020-04-12 17:50:29 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,7 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
+    birds = Bird.all
+    render json: birds #.to_json
+    # dont need instance varialbes since were working with json and not erb
   end
 end
